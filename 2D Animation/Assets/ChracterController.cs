e3f8ccc9cfa45cd466c21f6588486df86a3f85b0
DragonLady <cmlhc99@gmail.com>
2021-02-05 16:01:05 +1300

    Finished!

---
+++ 2D Animation/Assets/ChracterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChracterController : MonoBehaviour
+{
+
+    public Animator aController;
+    public Transform parent;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            aController.SetBool("walking", true);
+            parent.rotation = Quaternion.Euler(new Vector3(0, 180, 3));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            aController.SetBool("walking", true);
+            parent.rotation = Quaternion.Euler(new Vector3(0, 0, 3));
+
+        }
+        else
+        {
+            aController.SetBool("walking", false);
+            
+        }
+
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+
+
+            aController.SetTrigger("wingflapT");
+                
+        }
+
+
+    }
+
+   
+
+}
